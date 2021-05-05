606c726fe58e7a9b6dbc48e9034f0c7ef08f8622
krishpranav <krisna.pranav@gmail.com>
2021-05-05 11:02:16 +0530

    update configs

---
+++ postexp/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Auth
+AUTH = 'b9c39a336bb97a9c9bda2b82bdaacff3'
+
+# dir to save output files
+ARCHIVE_DIR = 'archive'
+
+# server ip
+SERVER_IP = None
+
+# BEACON_INTERVAL = 300
+BEACON_INTERVAL = None
